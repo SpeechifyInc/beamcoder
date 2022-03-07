ef90b85eadf475459e8149d63e4c071ac5466a4b
Liam Dyer <liamcdyer@gmail.com>
2022-03-07 01:17:23 -0500

    Build whole ffmpeg for now

--- setup-ffmpeg.sh
+++ setup-ffmpeg.sh
@@ -2,6 +2,6 @@
 set -x
 
 cd $(dirname $0)/ffmpeg
-./configure --disable-autodetect --disable-doc --disable-everything --disable-programs --disable-static --disable-stripping --disable-debug --enable-gpl --enable-shared --enable-pic --enable-version3 --enable-decoder=libopus,libvorbis,mp3,pcm_s32le,pcm_s16le --enable-encoder=libopus,libvorbis,libmp3lame,pcm_s32le,pcm_s16le --enable-demuxer=mp3,ogg,wav --enable-muxer=mp3,ogg,wav --enable-filter=asetnsamples --enable-libmp3lame --enable-libopus --enable-libvorbis --prefix=build
+./configure --disable-autodetect --disable-doc --disable-programs --disable-static --disable-stripping --disable-debug --enable-gpl --enable-shared --enable-pic --enable-version3 --enable-libmp3lame --enable-libopus --enable-libvorbis --prefix=build
 make
 make install
