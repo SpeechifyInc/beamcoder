782ed321a98a136f1a0c9e0f5edcd862f6f79282
Liam Dyer <liamcdyer@gmail.com>
2022-03-06 23:02:33 -0500

    Use script dir for cd in ffmpeg

--- setup-ffmpeg.sh
+++ setup-ffmpeg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-cd ffmpeg
+cd $(dirname $0)/ffmpeg
 ./configure --disable-autodetect --disable-doc --disable-everything --disable-programs --disable-static --disable-stripping --disable-debug --enable-gpl --enable-shared --enable-pic --enable-version3 --enable-decoder=libopus,libvorbis,mp3,pcm_s32le,pcm_s16le --enable-encoder=libopus,libvorbis,libmp3lame,pcm_s32le,pcm_s16le --enable-demuxer=mp3,ogg,wav --enable-muxer=mp3,ogg,wav --enable-filter=asetnsamples --enable-libmp3lame --enable-libopus --enable-libvorbis --prefix=build
 make
 make install-libs
